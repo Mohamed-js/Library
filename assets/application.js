c65e8a772107f6e72798636810e3a6ae97ffe2cb
alvaro <aaruizf@unal.edu.co>
2021-03-22 17:23:36 -0500

    Add new book and delete book functionality

--- assets/application.js
+++ assets/application.js
@@ -3,9 +3,9 @@ let title1 = "The first Book"
 let title2 = "The second Book"
 let title3 = "The third Book"
 
-function Book(title, description= "...", author= "unknown", read = false) {
+function Book(title, nop = 0 , author= "unknown", read = false) {
     this.title = title;
-    this.description =  description;
+    this.nop =  nop;
     this.author = author;
     this.read = read
 }
@@ -13,39 +13,67 @@ function Book(title, description= "...", author= "unknown", read = false) {
 book1 = new Book(title1)
 book2 = new Book(title2)
 book3 = new Book(title3)
-// console.log(book)
 
-function addBookToLibrary(book) {
-    myLibrary.push(book)
+function addBookToLibrary() {
+
+    let title = document.getElementById('title').value
+    let nop = document.getElementById('nop').value;
+    let author = document.getElementById('author').value;
+    // console.log(title);
+    book = new Book(title, nop, author);
+    myLibrary.push(book);
+    displayBooks();
+    hideForm();
 }
 
-addBookToLibrary(book1)
-addBookToLibrary(book2)
-addBookToLibrary(book3)
-
+function removeBook(book) {
+    myLibrary.splice(book,1);
+    displayBooks();
+}
 
 
 
 let cont = document.getElementById("container");
 
-var card = document.createElement("div");
+function displayBooks() {
+    cont.innerHTML = '';
+    for (i = 0; i < myLibrary.length; i++){
+        var card = document.createElement("div");
+        var head = document.createElement("h3");
+        var author = document.createElement("h6");
+        var read = document.createElement("h6");
+        var nop = document.createElement("p");
+        var deleteBook = document.createElement("button");
 
-var head = document.createElement("h3");
-var author = document.createElement("h6");
-var read = document.createElement("h6");
-var desc = document.createElement("p");
+        card.classList.add("card")
 
-card.classList.add("card")
+        head.textContent = myLibrary[i].title
+        author.textContent = myLibrary[i].author
+        read.textContent = myLibrary[i].read
+        nop.textContent = myLibrary[i].nop
+        deleteBook.textContent = 'Remove this book';
+        deleteBook.setAttribute('onclick',`removeBook(${i})`);
 
-head.textContent = myLibrary[0].title
-author.textContent = myLibrary[0].author
-read.textContent = myLibrary[0].read
-desc.textContent = myLibrary[0].description
+        card.appendChild(head)
+        card.appendChild(author)
+        card.appendChild(nop)
+        card.appendChild(read)
+        card.appendChild(deleteBook);
 
-card.appendChild(head)
-card.appendChild(author)
-card.appendChild(desc)
-card.appendChild(read)
+        cont.appendChild(card)
 
-cont.appendChild(card)
+    }
+}
 
+function displayForm() {
+    document.getElementById('createBookForm').style.display = 'block';
+    document.getElementById('displayForm').style.display = 'none';
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('nop').value = '';
+}
+
+function hideForm() {
+    document.getElementById('createBookForm').style.display = 'none';
+    document.getElementById('displayForm').style.display = 'block';
+}
